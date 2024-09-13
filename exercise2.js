c4b4b7ff15aa59a8d383ee0d7e08deab2cb4353b
Hiroki Ueno <rockyueno@gmail.com>
2024-09-13 16:22:51 -0700

    exercise2

--- exercise2.js
+++ exercise2.js
@@ -1,5 +1,5 @@
 // Exercise 2: Create a Prototype with Chaining and Inheritance
-// Instructions: 
+// Instructions:
 // 1. Create a constructor function called "Device" with a "brand" property and a method "powerOn" that logs "[brand] device is now powered on."
 // 2. Create another constructor function called "Smartphone" that inherits from Device, and add a "model" property to Smartphone.
 // 3. Add a method "call" to Smartphone's prototype that logs "Calling from [brand] [model]..."
@@ -8,7 +8,33 @@
 
 // YOUR CODE HERE
 
+function Device(brand) {
+  this._brand = brand
+}
 
+Device.prototype.getBrand = function () {
+  return this._brand
+}
+
+Device.prototype.setBrand = function (brand) {
+  this._brand = brand
+}
+
+Device.prototype.powerOn = function() {
+  console.log(`${this._brand} device is now powered on.`);
+}
+
+function Smartphone(brand, model) {
+  Device.call(this, brand)
+  this._model = model
+}
+
+Smartphone.prototype = Object.create(Device.prototype)
+Smartphone.prototype.constructor = Device
+
+Smartphone.prototype.call = function () {
+  console.log(`Calling from ${Device.prototype.getBrand.call(this)} ${this._model}...`);
+}
 
 
 // Test the Smartphone prototype with inheritance
