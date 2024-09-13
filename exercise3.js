80b3485c8acc70279c2f55088bffbb659f19b700
Hiroki Ueno <rockyueno@gmail.com>
2024-09-13 16:28:33 -0700

    exercise3

--- exercise3.js
+++ exercise3.js
@@ -1,22 +1,35 @@
 // Exercise 3: Convert a Class to a Function Prototype
-// Instructions: 
+// Instructions:
 // 1. Below is a class called "Person". Your task is to convert this class into a constructor function with methods defined on its prototype.
 // 2. Ensure the functionality remains the same after converting.
 
 
-class Person {
-  constructor(firstName, lastName) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-  }
-  
-  getFullName() {
-    return `${this.firstName} ${this.lastName}`;
-  }
-  
-  greet() {
-    console.log(`Hello, my name is ${this.getFullName()}.`);
-  }
+// class Person {
+//   constructor(firstName, lastName) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//   }
+
+//   getFullName() {
+//     return `${this.firstName} ${this.lastName}`;
+//   }
+
+//   greet() {
+//     console.log(`Hello, my name is ${this.getFullName()}.`);
+//   }
+// }
+
+function Person(firstName, lastName) {
+  this._firstName = firstName
+  this._lastName = lastName
+}
+
+Person.prototype.getFullName = function () {
+  return `${this._firstName} ${this._lastName}`
+}
+
+Person.prototype.greet = function () {
+  console.log(`Hello, my name is ${this.getFullName()}.`);
 }
 
 
